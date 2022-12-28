82862dfd164342cb74fae23c0384ffcbd90cb19c
Andrei Kazakov <nvgator@gmail.com>
2022-12-28 22:33:47 +0200

    form design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.RockButton = new System.Windows.Forms.Button();
+            this.ScissorsButton = new System.Windows.Forms.Button();
+            this.PaperButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RockButton
+            // 
+            this.RockButton.Location = new System.Drawing.Point(13, 13);
+            this.RockButton.Name = "RockButton";
+            this.RockButton.Size = new System.Drawing.Size(75, 60);
+            this.RockButton.TabIndex = 0;
+            this.RockButton.Text = "Rock";
+            this.RockButton.UseVisualStyleBackColor = true;
+            // 
+            // ScissorsButton
+            // 
+            this.ScissorsButton.Location = new System.Drawing.Point(95, 13);
+            this.ScissorsButton.Name = "ScissorsButton";
+            this.ScissorsButton.Size = new System.Drawing.Size(75, 60);
+            this.ScissorsButton.TabIndex = 1;
+            this.ScissorsButton.Text = "Scissors";
+            this.ScissorsButton.UseVisualStyleBackColor = true;
+            // 
+            // PaperButton
+            // 
+            this.PaperButton.Location = new System.Drawing.Point(177, 13);
+            this.PaperButton.Name = "PaperButton";
+            this.PaperButton.Size = new System.Drawing.Size(75, 60);
+            this.PaperButton.TabIndex = 2;
+            this.PaperButton.Text = "Paper";
+            this.PaperButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(263, 84);
+            this.Controls.Add(this.PaperButton);
+            this.Controls.Add(this.ScissorsButton);
+            this.Controls.Add(this.RockButton);
+            this.Name = "Form1";
+            this.Text = "assignment6_AndreiKazakov";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button RockButton;
+        private System.Windows.Forms.Button ScissorsButton;
+        private System.Windows.Forms.Button PaperButton;
     }
 }
 
